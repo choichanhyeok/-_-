dffb9b315ec77411486a943551592a4a6711b515
hyeoking <cksgurwkd12@naver.com>
2025-01-19 14:50:51 +0900

    [#1] StatementData 클래스에 필요한 객체들 정의 - 중간 데이터 구조 형태로 넘겨주어야 할 데이터들을 정의

--- src/main/java/chapter1/리팩토링_실습/code/dto/StatementData.java
+++ src/main/java/chapter1/리팩토링_실습/code/dto/StatementData.java
@@ -1,6 +1,23 @@
 package chapter1.리팩토링_실습.code.dto;
 
-public class StatementData {
+import chapter1.리팩토링_실습.code.data.Performance;
 
+import java.util.List;
+
+public class StatementData {
+    private String customer;
+    private List<Performance> performances;
+
+    public StatementData(List<Performance> performances) {
+        this.performances = performances;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public List<Performance> getPerformances() {
+        return performances;
+    }
 
 }
