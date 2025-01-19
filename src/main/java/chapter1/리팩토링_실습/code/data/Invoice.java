cc81f8e24780630e25722f4525c8e0c26c909f51
hyeoking <cksgurwkd12@naver.com>
2025-01-19 14:43:32 +0900

    [#1] Invoice 객체 생성 - js의 Object를 따라하기 위해 Map과 Object를 사용했던 기존 데이터 관리를 클래스로 전환 - 그 중 Invoice 정보를 담을 클래스 생성

---
+++ src/main/java/chapter1/리팩토링_실습/code/data/Invoice.java
@@ -0,0 +1,21 @@
+package chapter1.리팩토링_실습.code.data;
+
+import java.util.List;
+
+public class Invoice {
+    private String customer;
+    private List<Performance> performances;
+
+    public Invoice(String customer, List<Performance> performances) {
+        this.customer = customer;
+        this.performances = performances;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public List<Performance> getPerformances() {
+        return performances;
+    }
+}
